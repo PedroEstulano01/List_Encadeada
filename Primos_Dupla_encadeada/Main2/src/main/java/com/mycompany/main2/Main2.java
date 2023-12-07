b6b866f002dce49dbe059928c3f78f5bc45d4b5d
Estul4z <108631250+PedroEstulano01@users.noreply.github.com>
2023-12-06 21:54:42 -0300

    Main2.java

--- Primos_Dupla_encadeada/Main2/src/main/java/com/mycompany/main2/Main2.java
+++ Primos_Dupla_encadeada/Main2/src/main/java/com/mycompany/main2/Main2.java
@@ -7,27 +7,27 @@ package com.mycompany.main2;
 import java.util.Random;
 
 public class Main2 {
-    public static void main(String[] args) {
-        Random aleatorio = new Random(); //randolas
-        int[] vetor = new int[1000];
-        ListaDupla lista = new ListaDupla();
+  public static void main(String[] args) {
+    Random aleatorio = new Random(); //randolas
+    int[] vetor = new int[1000];
+    ListaDupla lista = new ListaDupla();
 
-        for (int i = 0; i < 1000; i++) {
-            vetor[i] = aleatorio.nextInt(19999) - 9999; //vai jogar aleatorio um numero 
-            System.out.print(vetor[i] + " ");
-            lista.adicionar(vetor[i]);
-        }
-        System.out.println(); //resto para imprimir dps do código princiapl 
-
-        System.out.println("Lista em ordem crescente:"); 
-        lista.imprimirCrescente();
-
-        System.out.println("Lista em ordem decrescente:");
-        lista.imprimirDecrescente();
-
-        lista.removerPrimos();
-
-        System.out.println("Lista após remover números primos (em ordem crescente):");
-        lista.imprimirCrescente();
+    for (int i = 0; i < 1000; i++) {
+      vetor[i] = aleatorio.nextInt(19999) - 9999; //vai jogar aleatorio um numero 
+      System.out.print(vetor[i] + " ");
+      lista.adicionar(vetor[i]);
     }
+    System.out.println(); //resto para imprimir dps do código princiapl 
+
+    System.out.println("Lista em ordem crescente:"); 
+    lista.imprimirCrescente();
+
+    System.out.println("Lista em ordem decrescente:");
+    lista.imprimirDecrescente();
+
+    lista.removerPrimos();
+
+    System.out.println("Lista após remover números primos (em ordem crescente):");
+    lista.imprimirCrescente();
+  }
 }
